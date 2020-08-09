b5ef86933096fd02c1c100d7752b1e3978b69d6e
Anuradha patil <66302519+anuradha50@users.noreply.github.com>
2020-08-09 20:34:32 +0530

    Add files via upload

---
+++ MapWithJquery.js
@@ -0,0 +1,34 @@
+function precarica( img ) {
+    $( img ).each(function(){
+        $('<img/') [0].src = this;
+    });
+}
+precarica({
+    D:\Image\5aee215016cbfeaa1d2d4811128e38c2.gif
+    D:\Image\5aee215016cbfeaa1d2d4811128e38c2.gif
+    D:\Image\5aee215016cbfeaa1d2d4811128e38c2.gif
+    D:\Image\5aee215016cbfeaa1d2d4811128e38c2.gif
+    D:\Image\5aee215016cbfeaa1d2d4811128e38c2.gif
+});
+
+/******** JQuery for external title ***********/
+
+jQuery(document).ready(function () {
+    $('.title-blue').mouseout(function() {
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png')
+    });
+    $('.title-blue').mouseover( function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-blue.png');
+        $('.title-blue').mouseover( function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-blue.png');
+        $('.title-blue').mouseover( function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-blue.png');
+        $('.title-blue').mouseover( function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-blue.png');
+        $('.title-blue').mouseover( function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-blue.png');
+        $('.title-blue').mouseover( function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-blue.png');
+
+    });
+});
